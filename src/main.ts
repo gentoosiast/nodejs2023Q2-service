edcac64bcfd6338bc9cf49ce8ba76811687d505f
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-26 09:28:00 +0300

    feat: add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { DEFAULT_PORT } from './shared/constants/env';
+import { version as appVersion } from 'package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? DEFAULT_PORT;
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home Library API: RS School Node.JS 2023 Q2 task')
+    .setVersion(appVersion)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(port);
 }
