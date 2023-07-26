c8c38fe7870cf4ff8ec88ab29a6e217f2469190f
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-26 09:01:20 +0300

    feat: use ConfigService to get PORT value from the environment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { DEFAULT_PORT } from './shared/constants/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? DEFAULT_PORT;
+
+  await app.listen(port);
 }
 bootstrap();
