46ec76c0540d27a4008de8cbb41ccd007cc2e5ae
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-31 16:53:08 +0300

    fix: fix handling of HttpExceptions in my global exception filter

--- src/shared/filters/global/global-exception.filter.ts
+++ src/shared/filters/global/global-exception.filter.ts
@@ -3,21 +3,16 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpExceptionBody,
   HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { UnknownIdException } from '@shared/exceptions/unknown-id.exception';
 
-interface ApiError {
-  message: string;
-  error: string;
-  statusCode: number;
-}
-
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: Error, host: ArgumentsHost) {
-    let body: ApiError;
+    let body: HttpExceptionBody;
     let status: HttpStatus;
     if (exception instanceof UnknownIdException) {
       const statusCode = HttpStatus.BAD_REQUEST;
@@ -31,11 +26,16 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     } else if (exception instanceof HttpException) {
       const statusCode = exception.getStatus();
 
-      body = {
-        message: exception.message,
-        error: exception.name,
-        statusCode,
-      };
+      const exceptionResponse = exception.getResponse();
+      if (typeof exceptionResponse !== 'string') {
+        body = exceptionResponse as HttpExceptionBody;
+      } else {
+        body = {
+          message: exceptionResponse,
+          error: exception.name,
+          statusCode,
+        };
+      }
       status = statusCode;
     } else {
       const statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
