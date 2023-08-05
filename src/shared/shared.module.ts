21a18ee801f7768444751a7426d4b4af2ef2a214
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-06 00:14:47 +0300

    feat: create PrismaService

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InMemoryDbService } from './services/in-memory-db.service';
+import { PrismaService } from './services/prisma.service';
 import { UUIDService } from './services/uuid.service';
 
 @Module({
-  providers: [InMemoryDbService, UUIDService],
-  exports: [InMemoryDbService, UUIDService],
+  providers: [InMemoryDbService, PrismaService, UUIDService],
+  exports: [InMemoryDbService, PrismaService, UUIDService],
 })
 export class SharedModule {}
