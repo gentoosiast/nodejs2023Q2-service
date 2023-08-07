a8325a3d10954ed2ba8a44b8083d6ec63e98de99
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-07 12:08:38 +0300

    refactor: remove no longer used UUIDServide & InMemoryDbService

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { InMemoryDbService } from './services/in-memory-db.service';
 import { PrismaService } from './services/prisma.service';
-import { UUIDService } from './services/uuid.service';
 
 @Module({
-  providers: [InMemoryDbService, PrismaService, UUIDService],
-  exports: [InMemoryDbService, PrismaService, UUIDService],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class SharedModule {}
