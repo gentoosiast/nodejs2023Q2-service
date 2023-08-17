449b2028262f6d249d972153a35d90cc882127a9
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-17 14:49:47 +0300

    fix: allow access without authorization to / endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { SkipAuth } from '@shared/decorators/public';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @SkipAuth()
   @ApiExcludeEndpoint()
   @Get()
   getHello(): string {
