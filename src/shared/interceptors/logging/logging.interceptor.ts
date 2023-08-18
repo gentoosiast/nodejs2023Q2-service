951f73f75fdc3aa1b76bec5871423c5b34d7b154
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-18 22:54:39 +0300

    feat: send messages from interceptor to custom logger

--- src/shared/interceptors/logging/logging.interceptor.ts
+++ src/shared/interceptors/logging/logging.interceptor.ts
@@ -2,17 +2,21 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
+  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(LoggingInterceptor.name);
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       tap((data) => {
         const responseBody = JSON.stringify(data);
-        console.log(`interceptor response body: ${responseBody}`);
+
+        this.logger.debug(responseBody);
       }),
     );
   }
