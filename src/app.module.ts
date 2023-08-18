debd0fdd11ae6a33769a7f5d382313ddf33fa364
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-18 19:34:11 +0300

    feat: implement interceptor to capture response body

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,6 +11,7 @@ import { FavsModule } from './favs/favs.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from '@shared/middlewares/logger/logger.middleware';
+import { LoggingInterceptor } from '@shared/interceptors/logging/logging.interceptor';
 
 @Module({
   imports: [
@@ -23,7 +25,10 @@ import { LoggerMiddleware } from '@shared/middlewares/logger/logger.middleware';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/shared/interceptors/logging/logging.interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      tap((data) => {
+        const responseBody = JSON.stringify(data);
+        console.log(`interceptor response body: ${responseBody}`);
+      }),
+    );
+  }
+}
