020c5602ca438fab44570ac7da71f52a9a70e6a9
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-18 23:41:10 +0300

    feat: display context, timestamp and use color in log messages

--- src/shared/services/custom-logger.service.ts
+++ src/shared/services/custom-logger.service.ts
@@ -1,44 +1,65 @@
-import { Injectable, LoggerService } from '@nestjs/common';
+import {
+  ConsoleLogger,
+  LogLevel as LogLevelName,
+  Injectable,
+  LoggerService,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from '@shared/intefaces/env-config';
 import { LogLevel } from '@shared/enums/log-level.enum';
 
 @Injectable()
-export class LoggingService implements LoggerService {
+export class LoggingService extends ConsoleLogger implements LoggerService {
   private logLevel = this.configService.get('LOGGER_LOG_LEVEL', {
     infer: true,
   });
-  constructor(private configService: ConfigService<EnvironmentVariables>) {}
+  constructor(private configService: ConfigService<EnvironmentVariables>) {
+    super();
+  }
 
   log(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Log) {
       return;
     }
-    console.log(`custom log: ${message}`);
+    this.outputMessage('log', optionalParams[0], message);
   }
+
   error(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Error) {
       return;
     }
-    console.log(`custom error: ${message}`);
+    this.outputMessage('error', optionalParams[0], message);
   }
+
   warn(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Warn) {
       return;
     }
-    console.log(`custom warn: ${message}`);
+    this.outputMessage('warn', optionalParams[0], message);
   }
-  debug?(message: any, ...optionalParams: any[]) {
+
+  debug(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Debug) {
       return;
     }
-    console.log(`custom debug: ${message}`);
+    this.outputMessage('debug', optionalParams[0], message);
   }
-  verbose?(message: any, ...optionalParams: any[]) {
+
+  verbose(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Verbose) {
       return;
     }
-    console.log(`custom verbose: ${message}`);
+    this.outputMessage('verbose', optionalParams[0], message);
   }
-  // setLogLevels?(levels: LogLevel[]) {}
+
+  private outputMessage(
+    levelName: LogLevelName,
+    context: string,
+    message: string,
+  ) {
+    const timeStamp = new Date().toLocaleString();
+    const output = `${timeStamp}: ${levelName.toUpperCase()} [${context}]: ${message}`;
+
+    console.log(this.colorize(output, levelName));
+  }
 }
