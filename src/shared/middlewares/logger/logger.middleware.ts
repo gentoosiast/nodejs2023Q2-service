1f462a9edf0df18a6f4308785141a2ef5cb090ac
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-18 19:37:48 +0300

    feat: capture response HTTP status code in the middleware

--- src/shared/middlewares/logger/logger.middleware.ts
+++ src/shared/middlewares/logger/logger.middleware.ts
@@ -4,10 +4,15 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    console.log(`request original url: ${req.originalUrl}`);
     console.log(`request url: ${req.baseUrl}`);
     console.log(`request method: ${req.method}`);
     console.log(`request query parameters: ${JSON.stringify(req.query)}`);
     console.log(`request body: ${JSON.stringify(req.body)}`);
+
+    res.on('finish', () => {
+      console.log(`response status: ${res.statusCode}`);
+    });
     next();
   }
 }
