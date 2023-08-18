7084eed47cfa7eb5050ef8f3e9d60d5e9be1ffa6
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-18 22:47:25 +0300

    feat: implement sending messages from middleware to logger

--- src/shared/middlewares/logger/logger.middleware.ts
+++ src/shared/middlewares/logger/logger.middleware.ts
@@ -1,17 +1,24 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(`request original url: ${req.originalUrl}`);
-    console.log(`request url: ${req.baseUrl}`);
-    console.log(`request method: ${req.method}`);
-    console.log(`request query parameters: ${JSON.stringify(req.query)}`);
-    console.log(`request body: ${JSON.stringify(req.body)}`);
+  private readonly logger = new Logger(LoggerMiddleware.name);
 
+  use(req: Request, res: Response, next: NextFunction) {
     res.on('finish', () => {
-      console.log(`response status: ${res.statusCode}`);
+      const { statusCode } = res;
+      const message = `${req.method} ${req.originalUrl} ${JSON.stringify(
+        req.body,
+      )} HTTP ${res.statusCode}`;
+
+      if (statusCode >= 500) {
+        this.logger.error(message);
+      } else if (statusCode >= 400) {
+        this.logger.log(message);
+      } else {
+        this.logger.debug(message);
+      }
     });
     next();
   }
