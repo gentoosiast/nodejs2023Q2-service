f6b03e4e08de0b09790b38a7687ab74a2548f60b
Sergey Koksharov <gentoosiastic@gmail.com>
2023-08-19 14:47:52 +0300

    feat: implement basic logging to files (w/o log rotation)

--- src/shared/services/custom-logger.service.ts
+++ src/shared/services/custom-logger.service.ts
@@ -5,61 +5,87 @@ import {
   LoggerService,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as path from 'node:path';
+import * as fsPromises from 'node:fs/promises';
+import * as os from 'node:os';
 import { EnvironmentVariables } from '@shared/intefaces/env-config';
 import { LogLevel } from '@shared/enums/log-level.enum';
 
 @Injectable()
 export class LoggingService extends ConsoleLogger implements LoggerService {
+  private combinedLogFilename = 'current-combined.log';
+  private errorLogFilename = 'current-error.log';
+  private logDirectory = path.join(process.cwd(), 'logs');
   private logLevel = this.configService.get('LOGGER_LOG_LEVEL', {
+    infer: true,
+  });
+  private maxLogSize = this.configService.get('LOGGER_MAX_FILE_SIZE', {
     infer: true,
   });
   constructor(private configService: ConfigService<EnvironmentVariables>) {
     super();
   }
 
-  log(message: any, ...optionalParams: any[]) {
+  async log(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Log) {
       return;
     }
-    this.outputMessage('log', optionalParams[0], message);
+    await this.outputMessage('log', message, optionalParams[0]);
   }
 
-  error(message: any, ...optionalParams: any[]) {
+  async error(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Error) {
       return;
     }
-    this.outputMessage('error', optionalParams[0], message);
+    await this.outputMessage('error', message, optionalParams[0]);
   }
 
-  warn(message: any, ...optionalParams: any[]) {
+  async warn(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Warn) {
       return;
     }
-    this.outputMessage('warn', optionalParams[0], message);
+    await this.outputMessage('warn', message, optionalParams[0]);
   }
 
-  debug(message: any, ...optionalParams: any[]) {
+  async debug(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Debug) {
       return;
     }
-    this.outputMessage('debug', optionalParams[0], message);
+    await this.outputMessage('debug', message, optionalParams[0]);
   }
 
-  verbose(message: any, ...optionalParams: any[]) {
+  async verbose(message: any, ...optionalParams: any[]) {
     if (this.logLevel > LogLevel.Verbose) {
       return;
     }
-    this.outputMessage('verbose', optionalParams[0], message);
+    await this.outputMessage('verbose', message, optionalParams[0]);
   }
 
-  private outputMessage(
+  private async outputMessage(
     levelName: LogLevelName,
-    context: string,
     message: string,
+    context = 'UnknownContext',
   ) {
     const timeStamp = new Date().toLocaleString();
     const output = `${timeStamp}: ${levelName.toUpperCase()} [${context}]: ${message}`;
 
     console.log(this.colorize(output, levelName));
+    await this.writeToLog(output, levelName);
+  }
+
+  private async writeToLog(output: string, levelName: LogLevelName) {
+    const logFilePath = path.join(this.logDirectory, this.combinedLogFilename);
+    const errorLogFilePath = path.join(
+      this.logDirectory,
+      this.errorLogFilename,
+    );
+    const logOutput = output + os.EOL;
+
+    const promises = [fsPromises.appendFile(logFilePath, logOutput)];
+    if (levelName === 'error') {
+      promises.push(fsPromises.appendFile(errorLogFilePath, logOutput));
+    }
+
+    await Promise.all(promises);
   }
 }
